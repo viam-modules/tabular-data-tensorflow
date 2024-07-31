4becbd0da2ab26c28b41dc9ff3f100c87fb0c603
Tahiya Salam <tahiyasalam@gmail.com>
2024-07-31 14:16:48 -0400

    Add secrets to test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,5 +3,5 @@
 python3 setup.py sdist --formats=gztar
 pip install dist/model-0.1.tar.gz
 
-API_KEY=$VIAM_DEV_API_KEY API_KEY_ID=$VIAM_DEV_API_KEY_ID python3 -m model.training --dataset_file=dataset.jsonl --model_output_directory=. --num_epochs=2
+python3 -m model.training --dataset_file=dataset.jsonl --model_output_directory=. --num_epochs=2
 pip install pytest && pytest tests/
